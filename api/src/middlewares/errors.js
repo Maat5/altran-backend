fcf1d0d4a4c66dab3cd9f51aee962f1612a4741d
Manuel Abrante <abrantemanuel@gmail.com>
2017-09-02 17:22:12 -0400

    create error handle middleware

---
+++ api/src/middlewares/errors.js
@@ -0,0 +1,28 @@
+const { getRequestsErrors } = require('./../helpers').Utils;
+
+module.exports = (err, req, res, next) => {
+  if (err) {
+    let getFields = (status, name) => {
+      return {
+        status: status,
+        name: name
+      }
+    };
+
+    let errorType = err.status === 400 && err.name === 'SyntaxError' && err.body ? 
+      getFields(400, 'invalidJson') : err.status === 404 ? 
+      getFields(404, 'notFound') : getFields(500, 'internal');
+
+    let exception = false;
+
+    if(errorType.name === 'internal')
+      exception = err;
+
+    let error = getRequestsErrors(errorType.name, errorType.status, exception);
+
+    return res
+      .status(error.statusCode)
+      .send(error.data);
+  }
+  next();
+};
