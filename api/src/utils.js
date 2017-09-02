0bf1cebe972aca5016912e8a9224b4cab095960d
Manuel Abrante <abrantemanuel@gmail.com>
2017-09-02 17:25:44 -0400

    Created internal router to handle helper functions , config and models

---
+++ api/src/utils.js
@@ -0,0 +1,10 @@
+
+const Models = require('../../models');
+const Config = require('../../config/config');
+const Helpers = require('./helpers');
+
+module.exports = {
+  Models,
+  Config,
+  Helpers
+};
